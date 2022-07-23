5d10258f9138fa6db64e479ed2cb1297cceeca29
Lars Naurath <lars.naurath@gmail.com>
2022-07-23 18:52:45 +0200

    improve fps display to include smoothing

--- src/systems/ShowFpsSystem.ts
+++ src/systems/ShowFpsSystem.ts
@@ -3,11 +3,16 @@ import {Application, Text} from 'pixi.js';
 
 export const createShowFpsSystem = (app: Application) => {
   const fpsText = new Text("test", {fill: 0xffffff});
+  fpsText.x = 30;
+  fpsText.y = 30;
   app.stage.addChild(fpsText);
-
+  let i = 0;
+  const fpsBuffer: number[] = [];
 
   return (world: World) => {
-    fpsText.text = app.ticker.FPS.toFixed(0);
+    fpsBuffer[i++ % 200] = app.ticker.FPS;
+    const sum = fpsBuffer.reduce((c, v) => c + v, 0);
+    fpsText.text = (sum / fpsBuffer.length).toFixed(0);
 
     return world;
   }
