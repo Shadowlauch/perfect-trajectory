e8441848d072155a46f6306407a25edd48c72734
Georgii Zhulikov <l8doku@gmail.com>
2022-07-26 18:16:03 +0300

    Fix rebasing errors

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,6 @@ import {createPlayerHitSystem} from './systems/PlayerHitSystem';
 import {createTweenSystem} from './systems/TweenSystem';
 import { Stage1 } from './configs/stages/Stage1';
 import {createAnimatedSpriteSystem} from './systems/AnimatedSpriteSystem';
-import { Stage1 } from './configs/stages/Stage1';
 
 export interface World extends IWorld {
   time: {
